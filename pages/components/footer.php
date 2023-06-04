2d3fa20c5c155aa5410645f0e7f81046d96192bd
jmor <jeromemorales86@live.com>
2023-06-04 09:15:54 +0800

    separated footer.php to another folder, created home.php, head.php

---
+++ pages/components/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+            <div class="container-fluid d-flex justify-content-between">
+              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © bootstrapdash.com 2021</span>
+              <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+   
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/hoverable-collapse.js"></script>
+    <script src="../assets/js/misc.js"></script>
+ 
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
+  </body>
+</html>

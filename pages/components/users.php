4eb84f6e39fc728642d84c789d320bd01223c8f3
jmor <jeromemorales86@live.com>
2023-06-04 10:06:20 +0800

    added form, table, inc folder, config.php, connected to db

---
+++ pages/components/users.php
@@ -0,0 +1,75 @@
+<div class="row">
+
+    <!-- form start -->
+    <div class="col-md-4 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+            <h4 class="card-title">Registration Form</h4>
+            <p class="card-description"> Basic form layout </p>
+                <form class="forms-sample">
+                    <div class="form-group">
+                    <label for="exampleInputUsername1">Username</label>
+                    <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputConfirmPassword1">Confirm Password</label>
+                    <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-check form-check-flat form-check-primary">
+                    <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input"> Remember me </label>
+                    </div>
+                    <button type="submit" class="btn btn-gradient-primary me-2">Submit</button>
+                    <button class="btn btn-light">Cancel</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- form end -->
+
+    <!-- table start -->
+    <div class="col-lg-8 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+            <h4 class="card-title">List of Users</h4>
+            </p>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th> User </th>
+                    <th> First name </th>
+                    <th> Progress </th>
+                    <th> Amount </th>
+                    <th> Deadline </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="py-1">
+                    <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                    </td>
+                    <td> Herman Beck </td>
+                    <td>
+                    <div class="progress">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    </td>
+                    <td> $ 77.99 </td>
+                    <td> May 15, 2015 </td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+        </div>
+     <!-- table end -->
+</div>
+
---
+++ pages/inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "lib_db");
+
+
+if ($conn) {
+    echo "Connected Successfully Connected SuccessfullyConnected SuccessfullyConnected SuccessfullyConnected SuccessfullyConnected SuccessfullyConnected Successfully ";
+}
+
+
+
+?>
